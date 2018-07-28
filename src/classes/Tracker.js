a83b8e6a733b947888fd4936370d4cecb8384d71
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2018-07-28 21:44:44 +0300

    Keep improving the Tracker

--- src/classes/Tracker.js
+++ src/classes/Tracker.js
@@ -32,31 +32,108 @@ module.exports = class Tracker extends EventEmitter {
   }
 
   /**
-   * Get all entries in a chronological order.
-   * @return {Array} All saved entries.
+   * Get entries since a timestamp.
+   * @param  {Array}  entries An array of entries to filter.
+   * @param  {Number} since   Timestamp to filter entries by.
+   * @return {Array}          An array of matched entries.
    */
-  getAllEntries() {
-    return this._data.entries;
-  }
-
-  /**
-   * Get entries since a specified timestamp.
-   * @param {Number} since Limit the entries to only those after `since`.
-   */
-  getEntriesSince(since) {
+  _filterSince(entries, since) {
     const result = [];
-    let i = this._data.entries.length - 1;
+    let i = entries.length - 1;
 
     while (i !== -1) {
-      const entry = this._data.entries[i];
+      if (entries[i].timestamp < since) break;
 
-      if (entry.timestamp < since) break;
-
-      result.unshift(entry);
+      result.unshift(entries[i]);
       i -= 1;
     }
 
     return result;
+  }
+
+  /**
+   * Get entries.
+   * @param  {Object} [options]       Options.
+   * @param  {Number} [options.since] Filter entries to only those added after `since`.
+   * @return {Array}                  An array of matched entries.
+   */
+  getEntries({ since = null } = {}) {
+    let entries = this._data.entries;
+
+    if (since) {
+      entries = this._filterSince(entries, since);
+    }
+
+    return entries;
+  }
+
+  /**
+   * Get entries by type.
+   * @param  {String} type            The type to filter by.
+   * @param  {Object} [options]       Options.
+   * @param  {Number} [options.since] Filter entries to only those added after `since`.
+   * @return {Array}                  An array of matched entries.
+   */
+  getEntriesByType(type, { since = null } = {}) {
+    let entries = this._data.entriesByType[type] || [];
+
+    if (since) {
+      entries = this._filterSince(entries, since);
+    }
+
+    return entries;
+  }
+
+  /**
+   * Get entries by label.
+   * @param  {String} label           The label to filter by.
+   * @param  {Object} [options]       Options.
+   * @param  {Number} [options.since] Filter entries to only those added after `since`.
+   * @return {Array}                  An array of matched entries.
+   */
+  getEntriesByLabel(label, { since = null } = {}) {
+    let entries = this._data.entriesByLabel[label] || [];
+
+    if (since) {
+      entries = this._filterSince(entries, since);
+    }
+
+    return entries;
+  }
+
+  /**
+   * Add a new entry.
+   * @param {Object} _             Entry.
+   * @param {String} _.type        Type for the entry.
+   * @param {Array}  _.labels      Labels for the entry.
+   * @param {Number} [_.timestamp] Timestamp for the entry.
+   * @param {Object} [_.data]      Data for the entry.
+   */
+  addEntry({ type, labels, timestamp = Date.now(), data = {} } = {}) {
+    // Validate arguments
+    ow(type, ow.string.minLength(1));
+    ow(labels, ow.array.nonEmpty.ofType(ow.string.minLength(1)));
+    ow(timestamp, ow.number.greaterThan(0));
+
+    // Construct the new entry
+    const entry = { _id: uuidv1(), type, labels, timestamp, data };
+
+    // Ensure the type and label are created
+    this._data.entriesByType[type] = this._data.entriesByType[type] || [];
+
+    labels.forEach(label => {
+      this._data.entriesByLabel[label] = this._data.entriesByLabel[label] || [];
+    });
+
+    // Insert the entry to the data structure
+    this._data.entries.push(entry);
+    this._data.entriesById[entry._id] = entry;
+    this._data.entriesByType[type].push(entry);
+
+    labels.forEach(label => this._data.entriesByLabel[label].push(entry));
+
+    // Trigger the `save` event so that the updated data can be saved to eg. database
+    this.emit('save', this._data);
   }
 
   /**
@@ -99,40 +176,5 @@ module.exports = class Tracker extends EventEmitter {
     });
 
     return result;
-  }
-
-  /**
-   * Add a new entry.
-   * @param {Object} _             Entry.
-   * @param {String} _.type        Type for the entry.
-   * @param {Array}  _.labels      Labels for the entry.
-   * @param {Number} [_.timestamp] Timestamp for the entry.
-   * @param {Object} [_.data]      Data for the entry.
-   */
-  addEntry({ type, labels, timestamp = Date.now(), data = {} } = {}) {
-    // Validate arguments
-    ow(type, ow.string.minLength(1));
-    ow(labels, ow.array.nonEmpty.ofType(ow.string.minLength(1)));
-    ow(timestamp, ow.number.greaterThan(0));
-
-    // Construct the new entry
-    const entry = { _id: uuidv1(), type, labels, timestamp, data };
-
-    // Ensure the type and label are created
-    this._data.entriesByType[type] = this._data.entriesByType[type] || [];
-
-    labels.forEach(label => {
-      this._data.entriesByLabel[label] = this._data.entriesByLabel[label] || [];
-    });
-
-    // Insert the entry to the data structure
-    this._data.entries.push(entry);
-    this._data.entriesById[entry._id] = entry;
-    this._data.entriesByType[type].push(entry);
-
-    labels.forEach(label => this._data.entriesByLabel[label].push(entry));
-
-    // Trigger the `save` event so that the updated data can be saved to eg. database
-    this.emit('save', this._data);
   }
 };
--- test/tracker.test.js
+++ test/tracker.test.js
@@ -11,36 +11,68 @@ test('adds a new entry', () => {
 
   tracker.addEntry({ type: 'Test', labels: ['test_1', 'test_2'] });
 
-  expect(tracker.getAllEntries()).toHaveLength(1);
-  expect(tracker.getAllEntries()[0].labels).toEqual(['test_1', 'test_2']);
+  expect(tracker.getEntries()).toHaveLength(1);
+  expect(tracker.getEntries()[0].labels).toEqual(['test_1', 'test_2']);
 });
 
-test('gets entries since a timestamp', () => {
-  const tracker = new Tracker();
+describe('getting and manipulating entries', () => {
+  let tracker = new Tracker();
+  const entries = [
+    { type: 'Test1', timestamp: 10, labels: ['test_1'], data: { money: 10 } },
+    { type: 'Test2', timestamp: 20, labels: ['test_2'], data: { money: 5 } },
+    { type: 'Test1', timestamp: 30, labels: ['test_1'], data: { money: 10 } },
+    { type: 'Test1', timestamp: 40, labels: ['test_1'], data: { money: 10 } },
+    { type: 'Test2', timestamp: 50, labels: ['test_2'], data: { money: 5 } },
+  ];
 
-  [
-    { type: 'Test', timestamp: 50, labels: ['test_1'] },
-    { type: 'Test', timestamp: 100, labels: ['test_1'] },
-  ].forEach(entry => tracker.addEntry(entry));
+  entries.forEach(entry => tracker.addEntry(entry));
 
-  expect(tracker.getEntriesSince(75)).toHaveLength(1);
-});
+  test('gets all entries', () => {
+    expect(tracker.getEntries()).toHaveLength(entries.length);
+  });
 
-test('groups entries by label and gets total earned money', () => {
-  const tracker = new Tracker();
+  test('gets all entries since a timestamp', () => {
+    expect(tracker.getEntries({ since: 45 })).toHaveLength(
+      entries.filter(entry => entry.timestamp >= 45).length
+    );
+  });
 
-  [
-    { type: 'Test', labels: ['test_1'], data: { money: 10 } },
-    { type: 'Test', labels: ['test_1'], data: { money: 5 } },
-    { type: 'Test', labels: ['test_2'], data: { money: 25 } },
-    { type: 'Test', labels: ['test_2'], data: { money: 10 } },
-  ].forEach(entry => tracker.addEntry(entry));
+  test('gets entries for a type', () => {
+    expect(tracker.getEntriesByType('Test1')).toHaveLength(
+      entries.filter(entry => entry.type === 'Test1').length
+    );
+  });
 
-  const result = tracker.mapReduce(
-    entry => entry.labels[0],
-    ({ money }, entry) => ({ money: (money || 0) + entry.data.money })
-  );
+  test('gets entries for a type since a timestamp', () => {
+    expect(tracker.getEntriesByType('Test1', { since: 45 })).toHaveLength(
+      entries.filter(entry => entry.type === 'Test1' && entry.timestamp >= 45).length
+    );
+  });
 
-  expect(result.test_1.money).toBe(15);
-  expect(result.test_2.money).toBe(35);
+  test('gets entries for a label', () => {
+    expect(tracker.getEntriesByLabel('test_1')).toHaveLength(
+      entries.filter(entry => entry.labels.includes('test_1')).length
+    );
+  });
+
+  test('gets entries for a label since a timestamp', () => {
+    expect(tracker.getEntriesByLabel('test_1', { since: 45 })).toHaveLength(
+      entries.filter(entry => entry.labels.includes('test_1') && entry.timestamp >= 45).length
+    );
+  });
+
+  test('MapReduces entries since a timestamp', () => {
+    const monies = entries.reduce(
+      (sums, entry) => ({ ...sums, [entry.labels[0]]: sums[entry.labels[0]] + entry.data.money }),
+      { test_1: 0, test_2: 0 }
+    );
+
+    const result = tracker.mapReduce(
+      entry => entry.labels[0],
+      ({ money }, entry) => ({ money: (money || 0) + entry.data.money })
+    );
+
+    expect(result.test_1.money).toBe(monies.test_1);
+    expect(result.test_2.money).toBe(monies.test_2);
+  });
 });
