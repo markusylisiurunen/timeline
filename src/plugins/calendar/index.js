cbd8a37f210089eff31d0051e5529389e90795e2
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2018-08-06 23:19:43 +0300

    Make it wörk

--- src/plugins/calendar/index.js
+++ src/plugins/calendar/index.js
@@ -1,87 +1,133 @@
 /**
  * @overview Google Calendar plugin.
+ *
+ * Google Calendar plugin config:
+ *
+ * {
+ *   "credentials": "Object|null <stored credentials for this plugin>",
+ *   "calendarId": "String|null <id of the attached calendar>",
+ *   "colors": "Object <assigned color for each type of event>"
+ * }
  */
 
 const documentation = require('./documentation');
-const { requestDeviceAndUserCodes, pollAuthorization, revokeAuthorization } = require('./util');
-const { calendar } = require('../../config');
+const { authorization, calendars, events } = require('./util');
+const { name, calendar } = require('../../config');
 
 /** Authenticate with the user's Google Calendar. */
 const init = async (_, config) => {
-  console.log("Welcome! Let's set up the Google Calendar plugin.");
-
-  // Start the OAuth flow
-  let deviceAndUserCodes = null;
-
-  try {
-    deviceAndUserCodes = await requestDeviceAndUserCodes(calendar.clientId);
-  } catch (e) {
-    console.log('Oh snap :( Something went wrong.');
+  if (await authorization.isAuthorized(config)) {
+    console.log('Seems like you are already set up :)');
     return;
   }
 
-  // Show instructions to continue the authorisation
-  console.log(
-    `Open ${deviceAndUserCodes.verification_url} in a browser and enter ${
-      deviceAndUserCodes.user_code
-    } when prompted.`
-  );
+  console.log("Welcome! Let's set up the Google Calendar plugin.\n");
 
-  console.log('Waiting for your response...');
+  // Start the OAuth flow
+  const codes = await authorization.getCodes(calendar.clientId);
+
+  console.log(`Step 1. Open ${codes.verification_url} in a browser`);
+  console.log(`Step 2. Enter ${codes.user_code}`);
+  console.log('\nWaiting for your response...');
 
   // Start polling the Google OAuth server for the users response
   let interval = setInterval(async () => {
-    const { error, error_description: errorDescription, ...pollResult } = await pollAuthorization(
-      calendar.clientId,
-      calendar.clientSecret,
-      deviceAndUserCodes.device_code
-    );
+    try {
+      await (async () => {
+        let pollResult = null;
 
-    // User hasn't responded to the consent prompt yet
-    if (error && error === 'authorization_pending') return;
+        try {
+          pollResult = await authorization.poll(
+            calendar.clientId,
+            calendar.clientSecret,
+            codes.device_code
+          );
+        } catch (data) {
+          // User hasn't responded to the consent prompt yet
+          if (data.error && data.error === 'authorization_pending') return;
 
-    // User refused to grant access
-    if (error && error === 'access_denied') {
-      console.log('Looks like you refused to grant access. You can try again if it was a mistake.');
-    } else if (error) {
-      console.log("Oops :( Something doesn't seem right...");
-      console.log(`Error (${error}): ${errorDescription}`);
+          if (data.error) {
+            if (data.error === 'access_denied') {
+              console.log('You refused to give the needed permissions.');
+            } else {
+              console.log("Oops :( Something doesn't seem right...");
+            }
+
+            clearInterval(interval);
+            return;
+          }
+        }
+
+        clearInterval(interval);
+
+        console.log('Permissions granted. Checking for the calendar...');
+
+        // Access was granted
+        const credentials = {
+          accessToken: pollResult.access_token,
+          refreshToken: pollResult.refresh_token,
+          tokenType: pollResult.token_type,
+          expiresAt: Date.now() + pollResult.expires_in * 1000,
+        };
+
+        config.set('calendar.credentials', credentials);
+
+        // Ensure that there is a valid calendar to add the events to
+        const cals = await calendars.list(credentials);
+        let theCalendar = cals.find(({ summary }) => summary === name);
+
+        if (!theCalendar) {
+          theCalendar = await calendars.create(credentials, { summary: name });
+          console.log('A new calendar created.');
+        }
+
+        config.set('calendar.calendarId', theCalendar.id);
+
+        console.log('Calendar plugin is now ready to be used.');
+      })();
+    } catch (error) {
+      console.log(JSON.stringify(error));
+      clearInterval(interval);
     }
-
-    // Access granted
-    if (!error) {
-      config.set('calendar.authorization', {
-        accessToken: pollResult.access_token,
-        refreshToken: pollResult.refresh_token,
-        tokenType: pollResult.token_type,
-        expiresAt: Date.now() + pollResult.expires_in * 1000,
-      });
-
-      console.log('Google Calendar plugin is now ready.');
-    }
-
-    clearInterval(interval);
-  }, deviceAndUserCodes.interval * 1000);
+  }, codes.interval * 1000);
 };
 
 /** Reset the tokens for the user's Google Calendar. */
 const reset = async (_, config) => {
-  const token = config.get('calendar.authorization.accessToken');
+  const credentials = config.get('calendar.credentials');
 
-  if (token) await revokeAuthorization(token);
+  if (credentials) {
+    await authorization.revoke(credentials.accessToken);
 
-  config.delete('calendar.authorization');
+    config.set('calendar.credentials', null);
+    config.set('calendar.calendarId', null);
+  }
+
   console.log('Google Calendar is now reset.');
 };
 
 /** Insert new events to the user's Google Calendar. */
-const onAdd = () => {
-  console.log('Calendar: Event added.');
-};
+const onAdd = async (config, timeline, event) => {
+  if (!(await authorization.isAuthorized(config))) return;
 
-/** Update an existing event in the user's Google Calendar. */
-const onUpdate = () => {
-  console.log('Calendar: Event updated.');
+  let { credentials, calendarId, colors = {} } = config.get('calendar');
+
+  if (!colors[event.type]) {
+    config.set(
+      `calendar.colors.${event.type}`,
+      `#${Array.from({ length: 6 }).map(() => Math.floor(Math.random() * 10))}`
+    );
+  }
+
+  try {
+    await events.insert(credentials, calendarId, {
+      start: { dateTime: new Date(event.from).toISOString() },
+      end: { dateTime: new Date(event.to).toISOString() },
+      summary: event.description,
+    });
+  } catch (error) {
+    console.log(JSON.stringify(error));
+  }
 };
 
 module.exports = async (args, config, timeline) => {
@@ -93,6 +139,5 @@ module.exports = async (args, config, timeline) => {
     );
   });
 
-  timeline.on('event.add', onAdd);
-  timeline.on('event.update', onUpdate);
+  timeline.on('event.add', onAdd.bind(null, config, timeline));
 };
--- src/plugins/calendar/util.js
+++ src/plugins/calendar/util.js
@@ -5,52 +5,159 @@
 const querystring = require('querystring');
 const fetch = require('node-fetch');
 
-/**
- * Request device and user codes from Google's OAuth server.
- * @param  {String}          id This app's client id for Google APIs.
- * @return {Promise<Object>}    Response from Google OAuth server.
- */
-const requestDeviceAndUserCodes = async id => {
-  const url = 'https://accounts.google.com/o/oauth2/device/code';
-  const query = querystring.stringify({
-    client_id: id,
-    scope: 'https://www.googleapis.com/auth/calendar',
-  });
+// Authoriation functions
 
-  const req = await fetch(`${url}?${query}`, { method: 'POST' });
-  return req.json();
+const authorization = {
+  /**
+   * Request device and user codes from Google's OAuth server.
+   * @param  {String}          clientId Client id for this plugin.
+   * @return {Promise<Object>}          Google OAuth server's response.
+   */
+  async getCodes(clientId) {
+    const url = 'https://accounts.google.com/o/oauth2/device/code';
+    const query = querystring.stringify({
+      client_id: clientId,
+      scope: 'https://www.googleapis.com/auth/calendar',
+    });
+
+    const res = await fetch(`${url}?${query}`, { method: 'POST' });
+    const data = await res.json();
+
+    if (!res.ok) throw data;
+
+    return data;
+  },
+
+  /**
+   * Poll for the credentials.
+   * @param  {String}          clientId     Client id for this plugin.
+   * @param  {String}          clientSecret Client secret for this plugin.
+   * @param  {String}          deviceCode   The device code acquired previously.
+   * @return {Promise<Object>}              Google OAuth server's response.
+   */
+  async poll(clientId, clientSecret, deviceCode) {
+    const url = 'https://www.googleapis.com/oauth2/v4/token';
+    const query = querystring.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code: deviceCode,
+      grant_type: 'http://oauth.net/grant_type/device/1.0',
+    });
+
+    const res = await fetch(`${url}?${query}`, { method: 'POST' });
+    const data = await res.json();
+
+    if (!res.ok) throw data;
+
+    return data;
+  },
+
+  /**
+   * Revoke permissions.
+   * @param  {String}  accessToken Previously acquired access token.
+   * @return {Promise}             Resolved once revoked.
+   */
+  async revoke(accessToken) {
+    const url = 'https://accounts.google.com/o/oauth2/revoke';
+    const query = querystring.stringify({ token: accessToken });
+
+    const res = await fetch(`${url}?${query}`, { method: 'POST' });
+
+    if (!res.ok) throw new Error();
+  },
+
+  /**
+   * Check if the plugin is authorized and possibly refresh the access token.
+   * @param  {Configstore}      config App's configuration store.
+   * @return {Promise<Boolean>}        True if was authorized, otherwise false.
+   */
+  async isAuthorized(config) {
+    const credentials = config.get('calendar.credentials');
+
+    if (!credentials) return false;
+
+    // TODO: If credentials have expired, try to refresh.
+    if (credentials.expiresAt < Date.now()) return false;
+
+    return true;
+  },
 };
 
-/**
- * Poll for the authorization by the user.
- * @param  {String}          id     This app's client id for Google APIs.
- * @param  {String}          secret This app's client secret for Google APIs.
- * @param  {String}          code   The device code acquired previously.
- * @return {Promise<Object>}        Response from Google OAuth server.
- */
-const pollAuthorization = async (id, secret, code) => {
-  const url = 'https://www.googleapis.com/oauth2/v4/token';
-  const query = querystring.stringify({
-    client_id: id,
-    client_secret: secret,
-    code: code,
-    grant_type: 'http://oauth.net/grant_type/device/1.0',
-  });
+// Calendar functions
 
-  const req = await fetch(`${url}?${query}`, { method: 'POST' });
-  return req.json();
+const calendars = {
+  /**
+   * Get user's calendars.
+   * @param  {Object}         credentials Credentials for this plugin.
+   * @return {Promise<Array>}             The user's calendars.
+   */
+  async list(credentials) {
+    const url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList';
+    const headers = {};
+
+    headers['Authorization'] = `${credentials.tokenType} ${credentials.accessToken}`;
+    headers['Accept'] = 'application/json';
+
+    const res = await fetch(url, { headers });
+    const data = await res.json();
+
+    if (!res.ok) throw data;
+
+    return data.items;
+  },
+
+  /**
+   * Create a new calendar.
+   * @param  {Object}          credentials Credentials for this plugin.
+   * @param  {Object}          calendar    Calendar to create.
+   * @return {Promise<Object>}             The created calendar.
+   */
+  async create(credentials, calendar) {
+    const url = 'https://www.googleapis.com/calendar/v3/calendars';
+    const headers = {};
+
+    headers['Authorization'] = `${credentials.tokenType} ${credentials.accessToken}`;
+    headers['Accept'] = 'application/json';
+    headers['Content-Type'] = 'application/json';
+
+    const body = JSON.stringify(calendar);
+
+    const res = await fetch(url, { method: 'POST', headers, body });
+    const data = await res.json();
+
+    if (!res.ok) throw data;
+
+    return data;
+  },
 };
 
-/**
- * Revoke previously authorized permissions.
- * @param  {String}  token Previously acquired token.
- * @return {Promise}       Resolved if successful.
- */
-const revokeAuthorization = async token => {
-  const url = 'https://accounts.google.com/o/oauth2/revoke';
-  const query = querystring.stringify({ token });
+// Events functions
 
-  await fetch(`${url}?${query}`, { method: 'POST' });
+const events = {
+  /**
+   * Insert a new event to a calendar.
+   * @param  {Object}          credentials Credentials for this plugin.
+   * @param  {String}          calendarId  Calendar to insert to.
+   * @param  {Object}          event       Event to insert.
+   * @return {Promise<Object>}             The inserted event.
+   */
+  async insert(credentials, calendarId, event) {
+    const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`;
+    const headers = {};
+
+    headers['Authorization'] = `${credentials.tokenType} ${credentials.accessToken}`;
+    headers['Accept'] = 'application/json';
+    headers['Content-Type'] = 'application/json';
+
+    const body = JSON.stringify(event);
+
+    const res = await fetch(url, { method: 'POST', headers, body });
+    const data = await res.json();
+
+    if (!res.ok) throw data;
+
+    return data;
+  },
 };
 
-module.exports = { requestDeviceAndUserCodes, pollAuthorization, revokeAuthorization };
+module.exports = { authorization, calendars, events };
