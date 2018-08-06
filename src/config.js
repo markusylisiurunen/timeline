c9128dea9edfc747c9f7669310db1ea3d1c7c3a7
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2018-08-06 20:11:23 +0300

    Add calendar plugin's API client id and secret to config

--- src/config.js
+++ src/config.js
@@ -11,10 +11,18 @@ const configs = {};
 
 configs['dev'] = {
   name: `${pkg.name}-dev`,
+  calendar: {
+    clientId: '194536656505-2oo1khnbsihdi3o2loss3cl2ivm2gr3c.apps.googleusercontent.com',
+    clientSecret: 'CiFlb8GjY74tonEKHZGr-TTI',
+  },
 };
 
 configs['prod'] = {
   name: pkg.name,
+  calendar: {
+    clientId: '194536656505-2oo1khnbsihdi3o2loss3cl2ivm2gr3c.apps.googleusercontent.com',
+    clientSecret: 'CiFlb8GjY74tonEKHZGr-TTI',
+  },
 };
 
 // Export the correct config
