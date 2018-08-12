f4beb7dee8c16c585dbd3450c30d1448cbd75ee3
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2018-08-12 13:42:04 +0300

    Fix Google Spreadsheet date time format (#1)
    * Fix spreadsheet date time formats
    
    * Update Travis CI config
    
    * Add node_modules cache for Travis CI
    
    * Convert the serial number date format correctly
    
    * Make sure Google credentials are present before init

--- src/plugins/sheets/util.js
+++ src/plugins/sheets/util.js
@@ -6,6 +6,37 @@ const querystring = require('querystring');
 const fetch = require('node-fetch');
 
 /**
+ * Convert a serial number date to a Date instance.
+ * @param  {Number} serialNumberDate Date in the serial number format.
+ * @return {Date}                    Date instance.
+ */
+const convertSerialNumberDate = serialNumberDate => {
+  const daysSinceUnix = serialNumberDate - 25569;
+  return new Date(Math.round(daysSinceUnix * 86400 * 1000));
+};
+
+/**
+ * Get a spreadsheet.
+ * @param  {Object}          credentials   Credentials for Google API.
+ * @param  {String}          spreadsheetId Spreadsheet to get.
+ * @return {Promise<Object>}               The spreadsheet.
+ */
+const getSpreadsheet = async (credentials, spreadsheetId) => {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}`;
+  const headers = {};
+
+  headers['Authorization'] = `${credentials.tokenType} ${credentials.accessToken}`;
+  headers['Accept'] = 'application/json';
+
+  const res = await fetch(url, { headers });
+  const data = await res.json();
+
+  if (!res.ok) throw data;
+
+  return data;
+};
+
+/**
  * List events from the spreadsheet.
  * @param  {Object}         credentials   Credentials for Google API.
  * @param  {String}         spreadsheetId Spreadsheet to insert to.
@@ -17,6 +48,7 @@ const listEvents = async (credentials, spreadsheetId, sheetName) => {
   const query = querystring.stringify({
     majorDimension: 'ROWS',
     valueRenderOption: 'UNFORMATTED_VALUE',
+    dateTimeRenderOption: 'SERIAL_NUMBER',
   });
 
   const headers = {};
@@ -29,28 +61,32 @@ const listEvents = async (credentials, spreadsheetId, sheetName) => {
 
   if (!res.ok) throw data;
 
-  return data.values
-    ? data.values.map(([id, type, labels, from, to, description, data]) => ({
-        id,
-        type,
-        labels: labels.split(',').map(label => label.trim()),
-        from: new Date(from),
-        to: new Date(to),
-        description,
-        data: JSON.parse(data),
-      }))
-    : [];
+  if (!data.values || !data.values.length) return [];
+
+  return data.values.slice(1).map(row => {
+    let [id, type, labels, from, to, description, data] = row;
+
+    labels = labels.split(',').map(l => l.trim());
+
+    from = convertSerialNumberDate(from);
+    to = convertSerialNumberDate(to);
+
+    data = JSON.parse(data);
+
+    return { id, type, labels, from, to, description, data };
+  });
 };
 
 /**
  * Insert a new event to a spreadsheet.
  * @param  {Object}          credentials   Credentials for Google API.
+ * @param  {String}          locale        Locale to use for dates.
  * @param  {String}          spreadsheetId Spreadsheet to insert to.
  * @param  {String}          sheetName     Sheet to insert to.
  * @param  {Object}          event         Event to insert.
  * @return {Promise<Object>}               The inserted event.
  */
-const insertEvent = async (credentials, spreadsheetId, sheetName, event) => {
+const insertEvent = async (credentials, locale, spreadsheetId, sheetName, event) => {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}:append`;
   const query = querystring.stringify({
     valueInputOption: 'USER_ENTERED',
@@ -70,8 +106,8 @@ const insertEvent = async (credentials, spreadsheetId, sheetName, event) => {
         event.id,
         event.type,
         event.labels.join(', '),
-        new Date(event.from).toISOString(),
-        new Date(event.to).toISOString(),
+        new Date(event.from).toLocaleString(locale),
+        new Date(event.to).toLocaleString(locale),
         event.description,
         JSON.stringify(event.data),
       ],
@@ -86,4 +122,4 @@ const insertEvent = async (credentials, spreadsheetId, sheetName, event) => {
   return data;
 };
 
-module.exports = { listEvents, insertEvent };
+module.exports = { getSpreadsheet, listEvents, insertEvent };
