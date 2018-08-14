2d8a761103515c6e07066dc1a828687c6527b103
Markus Ylisiurunen <markus.ylisiurunen@intopalo.com>
2018-08-14 22:10:03 +0300

    Update the UI util module

--- src/util/ui.js
+++ src/util/ui.js
@@ -3,11 +3,25 @@
  */
 
 const chalk = require('chalk');
+const inquirer = require('inquirer');
+
+/**
+ * Ask a question from the user.
+ * @param  {Object}          question Question to ask.
+ * @return {Promise<Object>}          User's answer.
+ */
+const ask = async question => inquirer.prompt({ prefix: chalk.yellow('?'), ...question });
 
 /**
  * Print a message to the console.
  * @param {String} message Message to print.
  */
-const say = message => process.stdout.write(`${chalk.cyan('>')} ${message}`);
+const say = message => console.log(`${chalk.cyan('>')} ${message}`);
 
-module.exports = { say };
+/**
+ * Print an error to the console.
+ * @param {String} errorMessage Error message to print.
+ */
+const error = errorMessage => console.log(`${chalk.red('!')} ${errorMessage}`);
+
+module.exports = { ask, say, error };
