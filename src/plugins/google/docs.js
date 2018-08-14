f103b0c8fe50efed72a884d0a7a191dc371c9578
Markus Ylisiurunen <markus.ylisiurunen@intopalo.com>
2018-08-15 00:46:25 +0300

    Update the google plugin docs

--- src/plugins/google/docs.js
+++ src/plugins/google/docs.js
@@ -6,13 +6,13 @@ const docs = require('../../util/docs');
 
 // prettier-ignore
 module.exports = {
-  authenticate: docs.wrap([
-    docs.block.text('Usage: timeline google authorize'),
+  init: docs.wrap([
+    docs.block.text('Usage: timeline google init'),
     docs.block.text('Authorize the Google plugin with your Google account.'),
   ]),
 
-  revoke: docs.wrap([
-    docs.block.text('Usage: timeline google revoke'),
+  reset: docs.wrap([
+    docs.block.text('Usage: timeline google reset'),
     docs.block.text('Revoke any granted permissions from the Google plugin.'),
   ]),
 };
